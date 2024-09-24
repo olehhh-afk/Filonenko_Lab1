98b9b7d9e52a71fba567210d6cde9305f59d55f2
Oleh Filonenko <olegfill0007@gmail.com>
2024-09-24 14:40:34 +0300

    Update lab1.cpp

--- lab1.cpp
+++ lab1.cpp
@@ -1,28 +1,24 @@
-    #include <iostream>
-    #include <Windows.h>
-    using namespace std;
+#include <iostream>
+#include <Windows.h>
+using namespace std;
 
-    int main() {
-        SetConsoleCP(1251);
-        SetConsoleOutputCP(1251);
-        int age;
-
-    
-        cout << "Введіть ваш возраст: ";
-        cin >> age;
-
-    
-        if (age >= 0  && age <= 24) {
-            cout << "Молодьож" << endl;
-        }
-        else if (age >= 25 && age <= 64) {
-            cout << "Дорослі" << endl;
-        }
-        else if (age >= 65 && age <= 1200) {
-            cout << "Люди похилого віку" << endl;
-        }
-        else (age >= -1 && age <= -100) {
-            cout << "неправильн вказано" << endl;
-        }
-        return 0;
+int main() {
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    int age;
+    cout << "Введіть ваш возраст: ";
+    cin >> age;
+    if (age >= 0 && age <= 24) {
+        cout << "Молодьож" << endl;
     }
+    else if (age >= 25 && age <= 64) {
+        cout << "Дорослі" << endl;
+    }
+    else if (age >= 65 && age <= 1200) {
+        cout << "Люди похилого віку" << endl;
+    }
+    else {
+        cout << "неправильно вказано" << endl;
+    }
+    return 0;
+}
